9346bd79819ab4d884ee041e66394807e2b853c2
dunncc <dunncc@dukes.jmu.edu>
2026-05-04 18:57:26 -0400

    Update queryTwo.sql

--- queries/queryTwo.sql
+++ queries/queryTwo.sql
@@ -9,3 +9,12 @@ AND (
 WHERE Room.HotelID = 2
 AND RoomType.Name = 'Double'
 AND Occupies.OccupiesID IS NULL;
+
+INSERT INTO Occupies (OccupiesID, ReservationID, HotelID, RoomNumber, StartDateTime, EndDateTime)
+VALUES (5000, 2000, 2, 101, CURRENT_TIMESTAMP, NULL);
+
+INSERT INTO Occupant (OccupantID, OccupiesID, Name)
+VALUES (6000, 5000, 'Mrs. Smith');
+
+INSERT INTO Occupant (OccupantID, OccupiesID, Name)
+VALUES (6001, 5000, 'Mr. Smith');
